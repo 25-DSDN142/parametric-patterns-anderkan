8399fa817c5d98861744f1d3114b181bf166b115
anderkan <danp2002foster@gmail.com>
2025-07-22 12:10:31 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,6 +1,9 @@
 //your parameter variables go here!
 let rect_width  = 150;
 let rect_height = 150;
+let n = 10;
+let startN = 100;
+
 
 
 function setup_wallpaper(pWallpaper) {
@@ -18,13 +21,60 @@ function setup_wallpaper(pWallpaper) {
 
 function wallpaper_background() {
   background(240, 255, 240); //light honeydew green colour
+
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
+glyph(55,45,n,startN);
+glyph(145,45,n,startN);
+glyphNeg(55,155,n,startN);
+glyphNeg(145,155,n,startN);
+border();
+}
+
+function glyph(x,y,n,startN){
   noFill();
-  stroke(0);
+  stroke(36, 56, 212);
   strokeWeight(1);
+  for(let i = startN; i > 0; i -= n){
+    ellipse(x ,y+i, i, i);
+    strokeWeight(10 - (i*2));
+    stroke(36, 56, 212-i);
+  }
+}
+function glyphNeg(x,y,n,startN){
+  noFill();
+  stroke(36, 56, 212);
+  strokeWeight(1);
+  for(let i = startN; i > 0; i -= n){
+    ellipse(x ,y-i, i, i);
+    strokeWeight(10 - (i*2));
+    stroke(36, 56, 212-i);
+  }
 
-  ellipse(100 ,100, rect_width, rect_height);
+}
+function border(){
+  stroke(212, 92, 36);
+  strokeWeight(1);
+  rect(0,0,200,5);
+  pattern(0,0);
+  rect(0,0,5,200);
+  pattern(195,0);
+  rect(0,195,200,5);
+  pattern(0,195);
+  rect(195,0,5,200);
+  pattern(195,195);
+}
 
+function pattern(x,y){
+  stroke(0);
+  line(1+x,1+y,1+x,1+y);
+  line(4+x,1+y,4+x,1+y);
+  line(1+x,4+y,1+x,4+y);
+  line(4+x,4+y,4+x,4+y);
+  line(2+x,2+y,2+x,2+y);
+  line(3+x,3+y,3+x,3+y);
+  line(2+x,3+y,2+x,3+y);
+  line(3+x,2+y,3+x,2+y);
+  stroke(212, 92, 36);
 }
